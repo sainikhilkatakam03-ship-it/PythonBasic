532747dd7cb182d40a52abd919b4b7007573323d
sainikhilkatakam03-ship-it <sainikhilkatakam03@gmail.com>
2026-04-22 23:36:25 +0530

    Create Task4.py

---
+++ Task4.py
@@ -0,0 +1,6 @@
+# Task 4 : Write a for loop that prints numbers from 1 to 7. If the number is 5, stop the loop immediately using break.
+
+for i in range(1, 8):
+    if i == 5:
+        break
+    print(i)
